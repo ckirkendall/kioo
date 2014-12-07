5620e0a6d264a22c37a8748868be23bdbf8ffa90
Creighton Kirkendall <ckirkendall@gmail.com>
2014-12-07 09:25:26 -0500

    Adding better error messages resources that don't exist

---
+++ test/kioo/core_test.clj
@@ -0,0 +1,9 @@
+(ns kioo.core
+  (:require [clojure.test :as test :refer :all]
+            [kioo.core :as core :refer :all]))
+
+(deftest path-exists-test
+  (is (path-exists? "simple-div.html"))
+  (is (not (path-exists? "file-does-not-exist.txt")))
+  (is (path-exists? :not-a-path)))
+
