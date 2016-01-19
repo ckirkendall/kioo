30ddfe68185b2d22a989d587fd73bc0832a2cd50
Ales Guzik <me@aguzik.net>
2016-01-19 23:01:26 +0300

    fix lack of kioo.core/value-component on server

--- src/kioo/server/core.clj
+++ src/kioo/server/core.clj
@@ -101,35 +101,35 @@
 
 (defmacro snippet
   ([path sel args]
-     (snippet* path sel {} args server-emit-opts true))
+     (snippet* path sel {} args server-emit-opts false))
   ([path sel args trans]
-     (snippet* path sel trans args server-emit-opts true))
+     (snippet* path sel trans args server-emit-opts false))
   ([path sel args trans opts]
-     (snippet* path sel trans args (merge server-emit-opts opts) true)))
+     (snippet* path sel trans args (merge server-emit-opts opts) false)))
 
 (defmacro template
   ([path args]
-     (snippet* path [:body :> any-node] {} args server-emit-opts true))
+     (snippet* path [:body :> any-node] {} args server-emit-opts false))
   ([path args trans]
-     (snippet* path [:body :> any-node] trans args server-emit-opts true))
+     (snippet* path [:body :> any-node] trans args server-emit-opts false))
   ([path args trans opts]
-     (snippet* path [:body :> any-node] trans args (merge server-emit-opts opts) true)))
+     (snippet* path [:body :> any-node] trans args (merge server-emit-opts opts) false)))
 
 (defmacro defsnippet
   ([sym path sel args]
-     `(def ~sym ~(snippet* path sel {} args server-emit-opts true)))
+     `(def ~sym ~(snippet* path sel {} args server-emit-opts false)))
   ([sym path sel args trans]
-     `(def ~sym ~(snippet* path sel trans args server-emit-opts true)))
+     `(def ~sym ~(snippet* path sel trans args server-emit-opts false)))
   ([sym path sel args trans opts]
-     `(def ~sym ~(snippet* path sel trans args (merge server-emit-opts opts) true))))
+     `(def ~sym ~(snippet* path sel trans args (merge server-emit-opts opts) false))))
 
 (defmacro deftemplate
   ([sym path args]
-     `(def ~sym ~(snippet* path [:body :> any-node] {} args server-emit-opts true)))
+     `(def ~sym ~(snippet* path [:body :> any-node] {} args server-emit-opts false)))
   ([sym path args trans]
-     `(def ~sym ~(snippet* path [:body :> any-node] trans args server-emit-opts true)))
+     `(def ~sym ~(snippet* path [:body :> any-node] trans args server-emit-opts false)))
   ([sym path args trans opts]
-     `(def ~sym ~(snippet* path [:body :> any-node] trans args (merge server-emit-opts opts) true))))
+     `(def ~sym ~(snippet* path [:body :> any-node] trans args (merge server-emit-opts opts) false))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; TRANSFORMS
