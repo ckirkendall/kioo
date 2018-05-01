beb59a1376ee24aa79cea37c3d4f2d2902cd4ede
Jochen Rau <jrau@infinitecloud.com>
2018-05-01 11:33:27 -0400

    Downgrade org.clojure/clojurescript to 1.9.946
    This is to avoid "Undefined nameToPath" errors on recompile via cljsbuild.
    It signals the library user to provide this version (the scope is "provided").

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [org.omcljs/om "1.0.0-beta2" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server] :scope "provided"]
                  [reagent "0.8.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server] :scope "provided"]
                  [com.googlecode.htmlcompressor/htmlcompressor "1.5.2"]
